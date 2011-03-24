onox/OpenRTI

commit d96186c1068972c3e0c4175dbfea61e6ed4c9045

Simplify the ThreadRegistry.

src/OpenRTI/StreamProtocol.cpp
@@ -262,7 +262,7 @@ public:
     ConnectServerCallback(const SharedPtr<AbstractMessageSender>& messageSender) :
       _messageSender(messageSender)
     { }
-    virtual void exec(ThreadRegistry& threadRegistry, NamedThread& thread)
+    virtual void exec(NamedThread& thread)
     {
       _messageSender = static_cast<ServerThread&>(thread).connectServer(_messageSender);
     }
@@ -286,11 +286,11 @@ public:
     DisconnectServerCallback(const SharedPtr<AbstractMessageSender>& messageSender) :
       _messageSender(messageSender)
     { }
-    virtual void exec(ThreadRegistry& threadRegistry, NamedThread& thread)
+    virtual void exec(NamedThread& thread)
     {
       bool hasChildConnects = static_cast<ServerThread&>(thread).disconnectServer(_messageSender);
       if (!hasChildConnects)
-        thread.stopThread(threadRegistry);
+        thread.stopThread();
     }
     SharedPtr<AbstractMessageSender> _messageSender;
   };

src/OpenRTI/ThreadRegistry.h
@@ -52,7 +52,7 @@ public:
   class ThreadProcedureCallback : public Referenced {
   public:
     virtual ~ThreadProcedureCallback() {}
-    virtual void exec(ThreadRegistry& threadRegistry, NamedThread& thread) = 0;
+    virtual void exec(NamedThread& thread) = 0;
   };
 
   /// This one is to trigger ThreadProcedureCallbacks in the thread
@@ -114,23 +114,26 @@ public:
         // This just makes the dispatcher return from exec by setting
         // the dispatcher itself to 'done'. So when restarting exec,
         // make sure we set the dispatchers back to 'undone'.
-        _dispatcher.setDone(false);
-        _dispatcher.exec();
+        exec();
 
         // Just always when we get here, ask the parent to execute a callback.
         // In the non error case this is the only occation to get here
         registry->execCallback(*this);
       }
+
+      registry->deregisterThread(*this);
     }
 
-    void stopThread(ThreadRegistry& registry)
-    {
-      _done = true;
-      registry.deregisterThread(*this);
-    }
-
-    void wakeUp()
+    virtual void wakeUp()
     { _socketWakeupTrigger->trigger(); }
+    virtual void exec()
+    {
+      _dispatcher.setDone(false);
+      _dispatcher.exec();
+    }
+
+    void stopThread()
+    { _done = true; }
 
     const std::wstring& getName() const
     { return _name; }
@@ -204,8 +207,8 @@ public:
 
   class ThreadStopCallback : public ThreadProcedureCallback {
   public:
-    virtual void exec(ThreadRegistry& threadRegistry, NamedThread& thread)
-    { thread.stopThread(threadRegistry); }
+    virtual void exec(NamedThread& thread)
+    { thread.stopThread(); }
   };
 
   void destroyThread(const std::wstring& name)
@@ -295,7 +298,7 @@ private:
     threadProcedureCallback.swap(_threadProcedureCallback);
     if (threadProcedureCallback.valid()) {
       ScopeUnlock scopeUnlock(_registerMutex);
-      threadProcedureCallback->exec(*this, thread);
+      threadProcedureCallback->exec(thread);
     }
     _condition.signal();
   }

commit 4db02e8b0c8df2be6f00c0fb37ae76390a73897b

Fix a race condition when stopping a socket thread.
Partially revert d96186c1068972c3e0c4175dbfea61e6ed4c9045,
which introduced a race condition on atomically stopping
a socket server thread.

src/OpenRTI/StreamProtocol.cpp
@@ -80,7 +80,7 @@ public:
       _messageSender(messageSender),
       _clientOptions(clientOptions)
     { }
-    virtual void exec(NamedThread& thread)
+    virtual void exec(ThreadRegistry& threadRegistry, NamedThread& thread)
     {
       _messageSender = static_cast<ServerThread&>(thread).connectServer(_messageSender, _clientOptions);
     }
@@ -108,11 +108,11 @@ public:
     DisconnectServerCallback(const SharedPtr<AbstractMessageSender>& messageSender) :
       _messageSender(messageSender)
     { }
-    virtual void exec(NamedThread& thread)
+    virtual void exec(ThreadRegistry& threadRegistry, NamedThread& thread)
     {
       bool hasChildConnects = static_cast<ServerThread&>(thread).disconnectServer(_messageSender);
       if (!hasChildConnects)
-        thread.stopThread();
+        thread.stopThread(threadRegistry);
     }
     SharedPtr<AbstractMessageSender> _messageSender;
   };

src/OpenRTI/ThreadRegistry.h
@@ -48,7 +48,7 @@ public:
   class ThreadProcedureCallback : public Referenced {
   public:
     virtual ~ThreadProcedureCallback() {}
-    virtual void exec(NamedThread& thread) = 0;
+    virtual void exec(ThreadRegistry& threadRegistry, NamedThread& thread) = 0;
   };
 
   // The execution thread type that is managed with this registry
@@ -84,15 +84,16 @@ public:
         // In the non error case this is the only occation to get here
         registry->execCallback(*this);
       }
-
-      registry->deregisterThread(*this);
     }
 
     virtual void wakeUp() = 0;
     virtual void exec() = 0;
 
-    void stopThread()
-    { _done = true; }
+    void stopThread(ThreadRegistry& registry)
+    {
+      _done = true;
+      registry.deregisterThread(*this);
+    }
 
     const std::wstring& getName() const
     { return _name; }
@@ -159,8 +160,8 @@ public:
 
   class ThreadStopCallback : public ThreadProcedureCallback {
   public:
-    virtual void exec(NamedThread& thread)
-    { thread.stopThread(); }
+    virtual void exec(ThreadRegistry& threadRegistry, NamedThread& thread)
+    { thread.stopThread(threadRegistry); }
   };
 
   void destroyThread(const std::wstring& name)
@@ -250,7 +251,7 @@ private:
     threadProcedureCallback.swap(_threadProcedureCallback);
     if (threadProcedureCallback.valid()) {
       ScopeUnlock scopeUnlock(_registerMutex);
-      threadProcedureCallback->exec(thread);
+      threadProcedureCallback->exec(*this, thread);
     }
     _condition.signal();
   }