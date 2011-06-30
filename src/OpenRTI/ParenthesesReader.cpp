8a4e5556588dabc0f757cc0861a206b0ad7323ea
Mathias Froehlich <Mathias.Froehlich@web.de>
2011-06-30 17:29:52 +0200

    Add a lisp like syntax parser.

---
+++ src/OpenRTI/ParenthesesReader.cpp
@@ -0,0 +1,141 @@
+/* -*-c++-*- OpenRTI - Copyright (C) 2010-2011 Mathias Froehlich
+ *
+ * This file is part of OpenRTI.
+ *
+ * OpenRTI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenRTI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpenRTI.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "ParenthesesReader.h"
+
+#include <istream>
+
+namespace OpenRTI {
+
+ParenthesesReader::ContentHandler::~ContentHandler()
+{
+}
+
+void
+ParenthesesReader::ContentHandler::startDocument()
+{
+}
+
+void
+ParenthesesReader::ContentHandler::endDocument()
+{
+}
+
+void
+ParenthesesReader::ContentHandler::startElement(const ParenthesesReader& parenthesesReader, const StringList& tokens)
+{
+}
+
+void
+ParenthesesReader::ContentHandler::endElement()
+{
+}
+
+ParenthesesReader::ErrorHandler::~ErrorHandler(void)
+{
+}
+
+void
+ParenthesesReader::ErrorHandler::error(const ParenthesesReader& parenthesesReader, const char* msg)
+{
+}
+
+ParenthesesReader::ParenthesesReader() :
+  _line(0),
+  _column(0)
+{
+}
+
+ParenthesesReader::~ParenthesesReader()
+{
+}
+
+bool
+ParenthesesReader::parse(std::istream& stream, ContentHandler& contentHandler, ErrorHandler& errorHandler)
+{
+  contentHandler.startDocument();
+
+  _line = 0;
+  _column = 0;
+
+  bool newToken = true;
+  bool allowToken = false;
+  StringList tokenList;
+  while (stream.good()) {
+    int c = stream.get();
+    ++_column;
+    switch (c) {
+    case -1:
+      break;
+
+    case '(':
+      if (allowToken) {
+        // flush the collected tokes so far
+        if (tokenList.empty()) {
+          errorHandler.error(*this, "Opening brace without any token before");
+          return false;
+        }
+        contentHandler.startElement(*this, tokenList);
+        tokenList.clear();
+      }
+      newToken = true;
+      allowToken = true;
+      break;
+
+    case ')':
+      // flush the collected tokes so far
+      if (!tokenList.empty()) {
+        contentHandler.startElement(*this, tokenList);
+        tokenList.clear();
+      }
+      contentHandler.endElement();
+      newToken = true;
+      allowToken = false;
+      break;
+
+    case '\n':
+      ++_line;
+      // fallthrough
+    case '\r':
+      _column = 0;
+      // fallthrough
+    case ' ':
+    case '\t':
+      newToken = true;
+      break;
+
+    default:
+      if (!allowToken) {
+        errorHandler.error(*this, "Non whitespace character past closing brace");
+        return false;
+      }
+
+      if (newToken) {
+        tokenList.push_back(std::string());
+        newToken = false;
+      }
+      tokenList.back().push_back(c);
+      break;
+    }
+  }
+  contentHandler.endDocument();
+  return true;
+}
+
+} // namespace OpenRTI
---
+++ src/OpenRTI/ParenthesesReader.h
@@ -0,0 +1,69 @@
+/* -*-c++-*- OpenRTI - Copyright (C) 2010-2011 Mathias Froehlich
+ *
+ * This file is part of OpenRTI.
+ *
+ * OpenRTI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenRTI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpenRTI.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef OpenRTI_ParenthesesReader_h
+#define OpenRTI_ParenthesesReader_h
+
+#include <iosfwd>
+
+#include "Export.h"
+#include "StringUtils.h"
+
+namespace OpenRTI {
+
+/// Parser for lisp like parentheses syntax files.
+/// Is used to parse rti 1.3 fed files.
+class OPENRTI_API ParenthesesReader {
+public:
+  class OPENRTI_API ContentHandler {
+  public:
+    virtual ~ContentHandler();
+
+    virtual void startDocument();
+    virtual void endDocument();
+
+    virtual void startElement(const ParenthesesReader& parenthesesReader, const StringList& tokens);
+    virtual void endElement();
+  };
+
+  class OPENRTI_API ErrorHandler {
+  public:
+    virtual ~ErrorHandler(void);
+
+    virtual void error(const ParenthesesReader& parenthesesReader, const char* msg);
+  };
+
+  ParenthesesReader();
+  ~ParenthesesReader();
+
+  bool parse(std::istream& stream, ContentHandler& contentHandler, ErrorHandler& errorHandler);
+
+  unsigned getLine() const
+  { return _line; }
+  unsigned getColumn() const
+  { return _column; }
+
+private:
+  unsigned _line;
+  unsigned _column;
+};
+
+} // namespace OpenRTI
+
+#endif
