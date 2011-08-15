4636b1f4d70165ee45d510ba1f18bc5e0d745301
Mathias Froehlich <Mathias.Froehlich@web.de>
2011-08-15 21:33:03 +0200

    Implement StreamBufferProtocol.
    The StreamBufferProtocol is supposed to provide a more packet
    based api just like the StreamSocketReadEvent/StreamSocketWriteEvent
    pair did in the old implementation.

---
+++ src/OpenRTI/StreamBufferProtocol.cpp
@@ -0,0 +1,146 @@
+/* -*-c++-*- OpenRTI - Copyright (C) 2009-2011 Mathias Froehlich
+ *
+ * This file is part of OpenRTI.
+ *
+ * OpenRTI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenRTI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpenRTI.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "StreamBufferProtocol.h"
+
+#include "LogStream.h"
+
+namespace OpenRTI {
+
+StreamBufferProtocol::StreamBufferProtocol()
+{
+  _inputIterator = _inputBuffer.byte_begin();
+  _outputIterator = _outputBuffer.byte_begin();
+}
+
+StreamBufferProtocol::~StreamBufferProtocol()
+{
+}
+
+void
+StreamBufferProtocol::read(AbstractProtocolSocket& protocolSocket)
+{
+  while (getEnableRead()) {
+    while (_inputIterator != _inputBuffer.byte_end()) {
+      ssize_t ret = protocolSocket.recv(BufferRange(_inputIterator, _inputBuffer.byte_end()), false);
+      if (ret == -1) {
+        // EAGAIN or similar in this socket abstraction.
+        // Serious error numbers are delivered as exceptions.
+        return;
+      }
+      if (ret == 0) {
+        // EOF in this socket abstraction.
+        // Serious error numbers are delivered as exceptions
+        // FIXME close does not help.
+        protocolSocket.close();
+        return;
+      }
+      _inputIterator += ret;
+      _inputIterator.skip_empty_chunks(_inputBuffer.byte_end());
+    }
+    
+    readPacket(_inputBuffer);
+    if (_inputIterator != _inputBuffer.byte_end())
+      continue;
+    
+    _inputBuffer.clear();
+  }
+}
+
+bool
+StreamBufferProtocol::getEnableRead() const
+{
+  return !getInputBufferComplete();
+}
+
+void
+StreamBufferProtocol::write(AbstractProtocolSocket& protocolSocket)
+{
+  while (getEnableWrite()) {
+    if (_outputBuffer.empty())
+      writePacket();
+    
+    while (_outputIterator != _outputBuffer.byte_end()) {
+      ssize_t ret = protocolSocket.send(ConstBufferRange(_outputIterator, _outputBuffer.byte_end()), getMoreToSend());
+      if (ret == -1) {
+        // Signals a message to big error. Currently unhandled ...
+        // Serious error numbers are delivered as exceptions.
+        protocolSocket.close();
+        Log(MessageCoding, Warning) << "Got an error code from sending to socket!" << std::endl;
+        return;
+      }
+      if (ret == 0) {
+        // EAGAIN or similar in this socket abstraction.
+        // Serious error numbers are delivered as exceptions.
+        return;
+      }
+      _outputIterator += ret;
+      _outputIterator.skip_empty_chunks(_outputBuffer.byte_end());
+    }
+    
+    // We are ready with this packet, reset state
+    _outputBuffer.clear();
+    _outputIterator = _outputBuffer.byte_begin();
+  }
+}
+
+bool
+StreamBufferProtocol::getEnableWrite() const
+{
+  return !getOutputBufferComplete() || getMoreToSend();
+}
+
+void
+StreamBufferProtocol::addReadBuffer(size_t size)
+{
+  OpenRTIAssert(size);
+  if (_inputIterator == _inputBuffer.byte_end())
+    _inputIterator = _inputBuffer.insert(_inputBuffer.end(), VariableLengthData(size));
+  else
+    _inputBuffer.push_back(VariableLengthData(size));
+}
+
+void
+StreamBufferProtocol::addWriteBuffer(const VariableLengthData& value)
+{
+  if (value.empty())
+    return;
+  if (_outputIterator == _outputBuffer.byte_end())
+    _outputIterator = _outputBuffer.insert(_outputBuffer.end(), value);
+  else
+    _outputBuffer.push_back(value);
+}
+
+VariableLengthData&
+StreamBufferProtocol::addWriteBuffer(size_t size)
+{
+  if (_outputIterator == _outputBuffer.byte_end())
+    _outputIterator = _outputBuffer.insert(_outputBuffer.end(), VariableLengthData(size));
+  else
+    _outputBuffer.push_back(VariableLengthData(size));
+  return _outputBuffer.back();
+}
+
+VariableLengthData&
+StreamBufferProtocol::addScratchWriteBuffer()
+{
+  return addWriteBuffer(0);
+}
+
+} // namespace OpenRTI
---
+++ src/OpenRTI/StreamBufferProtocol.h
@@ -0,0 +1,67 @@
+/* -*-c++-*- OpenRTI - Copyright (C) 2009-2011 Mathias Froehlich
+ *
+ * This file is part of OpenRTI.
+ *
+ * OpenRTI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenRTI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpenRTI.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef OpenRTI_StreamBufferProtocol_h
+#define OpenRTI_StreamBufferProtocol_h
+
+#include "AbstractProtocolLayer.h"
+#include "Buffer.h"
+
+namespace OpenRTI {
+
+class OPENRTI_API StreamBufferProtocol : public AbstractProtocolLayer {
+public:
+  StreamBufferProtocol();
+  virtual ~StreamBufferProtocol();
+
+  virtual void readPacket(const Buffer& buffer) = 0;
+  virtual void writePacket() = 0;
+  virtual bool getMoreToSend() const = 0;
+
+  virtual void read(AbstractProtocolSocket& protocolSocket);
+  virtual bool getEnableRead() const;
+
+  virtual void write(AbstractProtocolSocket& protocolSocket);
+  virtual bool getEnableWrite() const;
+
+  bool getBuffersComplete() const
+  { return getInputBufferComplete() && getOutputBufferComplete(); }
+  bool getInputBufferComplete() const
+  { return Buffer::const_byte_iterator(_inputIterator) == _inputBuffer.byte_end(); }
+  bool getOutputBufferComplete() const
+  { return _outputIterator == _outputBuffer.byte_end(); }
+
+  void addReadBuffer(size_t size);
+  void addWriteBuffer(const VariableLengthData& value);
+  VariableLengthData& addWriteBuffer(size_t size);
+  VariableLengthData& addScratchWriteBuffer();
+
+private:
+  // Buffer for the incomming data
+  Buffer _inputBuffer;
+  Buffer::byte_iterator _inputIterator;
+
+  // Buffer for the outgoing data
+  Buffer _outputBuffer;
+  Buffer::const_byte_iterator _outputIterator;
+};
+
+} // namespace OpenRTI
+
+#endif
