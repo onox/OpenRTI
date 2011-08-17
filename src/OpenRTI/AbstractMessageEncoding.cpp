fa92ab85f1176e3def36491529729730ab34861d
Mathias Froehlich <Mathias.Froehlich@web.de>
2011-08-17 22:37:07 +0200

    Add AbstractMessageEncoding.
    The AbstractMessageEncoding should replace the
    AbstractMessageEncoder/AbstractMessageDecoder pair.

---
+++ src/OpenRTI/AbstractMessageEncoding.cpp
@@ -0,0 +1,64 @@
+/* -*-c++-*- OpenRTI - Copyright (C) 2009-2011 Mathias Froehlich
+ *
+ * This file is part of OpenRTI.
+ *
+ * OpenRTI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenRTI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpenRTI.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "AbstractMessageEncoding.h"
+
+#include "Message.h"
+
+namespace OpenRTI {
+
+AbstractMessageEncoding::AbstractMessageEncoding()
+{
+}
+
+AbstractMessageEncoding::~AbstractMessageEncoding()
+{
+}
+
+bool
+AbstractMessageEncoding::getEnableRead() const
+{
+  return true;
+}
+
+void
+AbstractMessageEncoding::writePacket()
+{
+  SharedPtr<AbstractMessage> message = _connect->receive();
+  if (!message.valid())
+    return;
+  writeMessage(*message);
+}
+
+bool AbstractMessageEncoding::getMoreToSend() const
+{
+  return !_connect->empty();
+}
+
+void
+AbstractMessageEncoding::error(const Exception& e)
+{
+  /// FIXME!!!, just have a ConnectionClosed message. Depending on the server nodes state, it can decide what to do
+  SharedPtr<ConnectionLostMessage> message = new ConnectionLostMessage;
+  message->setFaultDescription(e.getReason());
+  _connect->send(message);
+  _connect->close();
+}
+
+} // namespace OpenRTI
---
+++ src/OpenRTI/AbstractMessageEncoding.h
@@ -0,0 +1,57 @@
+/* -*-c++-*- OpenRTI - Copyright (C) 2009-2011 Mathias Froehlich
+ *
+ * This file is part of OpenRTI.
+ *
+ * OpenRTI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenRTI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpenRTI.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef OpenRTI_AbstractMessageEncoding_h
+#define OpenRTI_AbstractMessageEncoding_h
+
+#include "AbstractConnect.h"
+#include "StreamBufferProtocol.h"
+
+namespace OpenRTI {
+
+class OPENRTI_API AbstractMessageEncoding : public StreamBufferProtocol {
+public:
+  AbstractMessageEncoding();
+  virtual ~AbstractMessageEncoding();
+
+  void setConnect(const SharedPtr<AbstractConnect>& connect)
+  { _connect = connect; }
+  const SharedPtr<AbstractConnect>& getConnect() const
+  { return _connect; }
+
+  /// Should return the name of the encoding
+  virtual const char* getName() const = 0;
+
+  /// Still to be implemented in the actual encodings
+  virtual void readPacket(const Buffer& buffer) = 0;
+  virtual void writeMessage(const AbstractMessage& message) = 0;
+
+  /// Already implemented here
+  virtual bool getEnableRead() const;
+  virtual void writePacket();
+  virtual bool getMoreToSend() const;
+  virtual void error(const Exception& e);
+
+protected:
+  SharedPtr<AbstractConnect> _connect;
+};
+
+} // namespace OpenRTI
+
+#endif
