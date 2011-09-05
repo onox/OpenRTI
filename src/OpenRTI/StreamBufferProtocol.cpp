onox/OpenRTI

commit 3535c74a4cb3dd2bd045c2f961ce0a5f1379166f

Reimplement pooling for scratch input/output buffers.

src/OpenRTI/StreamBufferProtocol.cpp
@@ -54,11 +54,17 @@ StreamBufferProtocol::read(AbstractProtocolSocket& protocolSocket)
       _inputIterator += ret;
       _inputIterator.skip_empty_chunks(_inputBuffer.byte_end());
     }
-    
+
     readPacket(_inputBuffer);
     if (_inputIterator != _inputBuffer.byte_end())
       continue;
-    
+
+    for (std::list<Buffer::iterator>::iterator i = _inputScratchBufferList.begin();
+         i != _inputScratchBufferList.end(); ++i) {
+      _scratchPool.splice(_scratchPool.end(), _inputBuffer, *i);
+    }
+
+    _iteratorPool.splice(_iteratorPool.end(), _inputScratchBufferList);
     _inputBuffer.clear();
   }
 }
@@ -75,7 +81,7 @@ StreamBufferProtocol::write(AbstractProtocolSocket& protocolSocket)
   while (getEnableWrite()) {
     if (_outputBuffer.empty())
       writePacket();
-    
+
     while (_outputIterator != _outputBuffer.byte_end()) {
       ssize_t ret = protocolSocket.send(ConstBufferRange(_outputIterator, _outputBuffer.byte_end()), getMoreToSend());
       if (ret == -1) {
@@ -93,9 +99,16 @@ StreamBufferProtocol::write(AbstractProtocolSocket& protocolSocket)
       _outputIterator += ret;
       _outputIterator.skip_empty_chunks(_outputBuffer.byte_end());
     }
-    
+
     // We are ready with this packet, reset state
+    for (std::list<Buffer::iterator>::iterator i = _outputScratchBufferList.begin();
+         i != _outputScratchBufferList.end(); ++i) {
+      _scratchPool.splice(_scratchPool.end(), _outputBuffer, *i);
+    }
+
+    _iteratorPool.splice(_iteratorPool.end(), _outputScratchBufferList);
     _outputBuffer.clear();
+
     _outputIterator = _outputBuffer.byte_begin();
   }
 }
@@ -117,6 +130,28 @@ StreamBufferProtocol::addReadBuffer(size_t size)
 }
 
 void
+StreamBufferProtocol::addScratchReadBuffer(size_t size)
+{
+  OpenRTIAssert(size);
+  VariableLengthDataList scratchElement;
+  if (!_scratchPool.empty())
+    scratchElement.splice(scratchElement.end(), _scratchPool, _scratchPool.begin());
+  else
+    scratchElement.push_back(VariableLengthData());
+  if (_inputIterator == _inputBuffer.byte_end())
+    _inputIterator = scratchElement.begin();
+
+  if (_iteratorPool.empty())
+    _inputScratchBufferList.push_back(scratchElement.begin());
+  else {
+    _inputScratchBufferList.splice(_inputScratchBufferList.end(), _iteratorPool, _iteratorPool.begin());
+    _inputScratchBufferList.back() = scratchElement.begin();
+  }
+  _inputBuffer.splice(_inputBuffer.end(), scratchElement, scratchElement.begin());
+  _inputBuffer.back().resize(size);
+}
+
+void
 StreamBufferProtocol::addWriteBuffer(const VariableLengthData& value)
 {
   if (value.empty())
@@ -128,19 +163,25 @@ StreamBufferProtocol::addWriteBuffer(const VariableLengthData& value)
 }
 
 VariableLengthData&
-StreamBufferProtocol::addWriteBuffer(size_t size)
-{
-  if (_outputIterator == _outputBuffer.byte_end())
-    _outputIterator = _outputBuffer.insert(_outputBuffer.end(), VariableLengthData(size));
-  else
-    _outputBuffer.push_back(VariableLengthData(size));
-  return _outputBuffer.back();
-}
-
-VariableLengthData&
 StreamBufferProtocol::addScratchWriteBuffer()
 {
-  return addWriteBuffer(0);
+  VariableLengthDataList scratchElement;
+  if (!_scratchPool.empty())
+    scratchElement.splice(scratchElement.end(), _scratchPool, _scratchPool.begin());
+  else
+    scratchElement.push_back(VariableLengthData());
+  if (_outputIterator == _outputBuffer.byte_end())
+    _outputIterator = scratchElement.begin();
+
+  if (_iteratorPool.empty())
+    _outputScratchBufferList.push_back(scratchElement.begin());
+  else {
+    _outputScratchBufferList.splice(_outputScratchBufferList.end(), _iteratorPool, _iteratorPool.begin());
+    _outputScratchBufferList.back() = scratchElement.begin();
+  }
+  _outputBuffer.splice(_outputBuffer.end(), scratchElement, scratchElement.begin());
+  _outputBuffer.back().resize(0);
+  return _outputBuffer.back();
 }
 
 } // namespace OpenRTI

src/OpenRTI/InitialClientStreamProtocol.cpp
@@ -65,7 +65,7 @@ InitialClientStreamProtocol::setConnectOptions(StringStringListMap connectOption
   writeOptionMap(connectOptions);
 
   // Add space for the initial header
-  addReadBuffer(12);
+  addScratchReadBuffer(12);
 }
 
 void

src/OpenRTI/InitialServerStreamProtocol.cpp
@@ -33,7 +33,7 @@ InitialServerStreamProtocol::InitialServerStreamProtocol(AbstractNetworkServer& 
   _networkServer(networkServer)
 {
   // Add space for the initial header
-  addReadBuffer(12);
+  addScratchReadBuffer(12);
 }
 
 InitialServerStreamProtocol::~InitialServerStreamProtocol()

src/OpenRTI/InitialStreamProtocol.cpp
@@ -117,8 +117,8 @@ void
 InitialStreamProtocol::writeOptionMap(const StringStringListMap& stringStringListMap)
 {
   // Header and body
-  EncodeStream headerStream(addWriteBuffer(0));
-  EncodeStream bodyStream(addWriteBuffer(0));
+  EncodeStream headerStream(addScratchWriteBuffer());
+  EncodeStream bodyStream(addScratchWriteBuffer());
 
   // Write the message body
   bodyStream.writeStringStringListMap(stringStringListMap);
@@ -134,7 +134,7 @@ InitialStreamProtocol::readPacket(const Buffer& buffer)
   switch (buffer.size()) {
   case 0:
     // First read a 12 byte header
-    addReadBuffer(12);
+    addScratchReadBuffer(12);
     break;
   case 1:
     {
@@ -155,7 +155,7 @@ InitialStreamProtocol::readPacket(const Buffer& buffer)
       }
 
       // Add buffer space for the message body
-      addReadBuffer(size - 12);
+      addScratchReadBuffer(size - 12);
     }
     break;
   case 2:

src/OpenRTI/StreamBufferProtocol.h
@@ -48,8 +48,8 @@ public:
   { return _outputIterator == _outputBuffer.byte_end(); }
 
   void addReadBuffer(size_t size);
+  void addScratchReadBuffer(size_t size);
   void addWriteBuffer(const VariableLengthData& value);
-  VariableLengthData& addWriteBuffer(size_t size);
   VariableLengthData& addScratchWriteBuffer();
 
 private:
@@ -60,6 +60,12 @@ private:
   // Buffer for the outgoing data
   Buffer _outputBuffer;
   Buffer::const_byte_iterator _outputIterator;
+
+  // Pool of scratch data packets
+  std::list<Buffer::iterator> _inputScratchBufferList;
+  std::list<Buffer::iterator> _outputScratchBufferList;
+  std::list<Buffer::iterator> _iteratorPool;
+  VariableLengthDataList _scratchPool;
 };
 
 } // namespace OpenRTI

src/OpenRTI/TightBE1MessageEncoding.cpp
@@ -3478,9 +3478,9 @@ TightBE1MessageEncoding::readPacket(const Buffer& buffer)
 {
   Buffer::const_iterator i = buffer.begin();
   if (i == buffer.end()) {
-    addReadBuffer(4);
+    addScratchReadBuffer(4);
   } else if (++i == buffer.end()) {
-    addReadBuffer(buffer.front().getUInt32BE(0));
+    addScratchReadBuffer(buffer.front().getUInt32BE(0));
   } else if (++i == buffer.end()) {
     decodeBody(*(--i));
   } else {

src/OpenRTI/codegen/messagegen.py
@@ -297,7 +297,7 @@ class MapDataType(DataType):
         t.resolveHasPayload(typeMap)
         self.__valueHasPayload = t.hasPayload()
         self._hasPayload = self.__keyHasPayload or self.__valueHasPayload
-        
+
     def keyHasPayload(self):
         return self.__keyHasPayload
 
@@ -1234,9 +1234,9 @@ class MessageEncoding(object):
         sourceStream.pushIndent()
         sourceStream.writeline('Buffer::const_iterator i = buffer.begin();')
         sourceStream.writeline('if (i == buffer.end()) {')
-        sourceStream.writeline('  addReadBuffer(4);')
+        sourceStream.writeline('  addScratchReadBuffer(4);')
         sourceStream.writeline('} else if (++i == buffer.end()) {')
-        sourceStream.writeline('  addReadBuffer(buffer.front().getUInt32BE(0));')
+        sourceStream.writeline('  addScratchReadBuffer(buffer.front().getUInt32BE(0));')
         sourceStream.writeline('} else if (++i == buffer.end()) {')
         sourceStream.writeline('  decodeBody(*(--i));')
         sourceStream.writeline('} else {')

commit bd1d79bf520273f1a15fad21aee420b9a4021938

Fix iterator usage in buffer pooling.
Buffer pooling 3535c74a4cb3dd2bd045c2f961ce0a5f1379166f
introduced this problem. Iterators to list members are invalid
past their pointee is spliced into an other list.

src/OpenRTI/StreamBufferProtocol.cpp
@@ -138,16 +138,17 @@ StreamBufferProtocol::addScratchReadBuffer(size_t size)
     scratchElement.splice(scratchElement.end(), _scratchPool, _scratchPool.begin());
   else
     scratchElement.push_back(VariableLengthData());
-  if (_inputIterator == _inputBuffer.byte_end())
-    _inputIterator = scratchElement.begin();
-
+  _inputBuffer.splice(_inputBuffer.end(), scratchElement, scratchElement.begin());
+  VariableLengthDataList::iterator back_iterator = _inputBuffer.end();
+  --back_iterator;
+  if (_inputIterator.iterator() == _inputBuffer.end())
+    _inputIterator = back_iterator;
   if (_iteratorPool.empty())
-    _inputScratchBufferList.push_back(scratchElement.begin());
+    _inputScratchBufferList.push_back(back_iterator);
   else {
     _inputScratchBufferList.splice(_inputScratchBufferList.end(), _iteratorPool, _iteratorPool.begin());
-    _inputScratchBufferList.back() = scratchElement.begin();
+    _inputScratchBufferList.back() = back_iterator;
   }
-  _inputBuffer.splice(_inputBuffer.end(), scratchElement, scratchElement.begin());
   _inputBuffer.back().resize(size);
 }
 
@@ -170,16 +171,17 @@ StreamBufferProtocol::addScratchWriteBuffer()
     scratchElement.splice(scratchElement.end(), _scratchPool, _scratchPool.begin());
   else
     scratchElement.push_back(VariableLengthData());
-  if (_outputIterator == _outputBuffer.byte_end())
-    _outputIterator = scratchElement.begin();
-
+  _outputBuffer.splice(_outputBuffer.end(), scratchElement, scratchElement.begin());
+  VariableLengthDataList::iterator back_iterator = _outputBuffer.end();
+  --back_iterator;
+  if (_outputIterator.iterator() == _outputBuffer.end())
+    _outputIterator = back_iterator;
   if (_iteratorPool.empty())
-    _outputScratchBufferList.push_back(scratchElement.begin());
+    _outputScratchBufferList.push_back(back_iterator);
   else {
     _outputScratchBufferList.splice(_outputScratchBufferList.end(), _iteratorPool, _iteratorPool.begin());
-    _outputScratchBufferList.back() = scratchElement.begin();
+    _outputScratchBufferList.back() = back_iterator;
   }
-  _outputBuffer.splice(_outputBuffer.end(), scratchElement, scratchElement.begin());
   _outputBuffer.back().resize(0);
   return _outputBuffer.back();
 }