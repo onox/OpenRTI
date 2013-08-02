ea2f4ef2b75b4f9de2c907cd4b404ae6da80a01f
Mathias Froehlich <Mathias.Froehlich@web.de>
2013-08-02 18:11:14 +0200

    Add an implementation file for NullFederateAmbassador.
    MSVC is an other time much more happy with this.

---
+++ src/rti1516/NullFederateAmbassador.cpp
@@ -0,0 +1,29 @@
+/* -*-c++-*- OpenRTI - Copyright (C) 2009-2012 Mathias Froehlich
+ *
+ * This file is part of OpenRTI.
+ *
+ * OpenRTI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenRTI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpenRTI.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// This time, the first include is above the api include.
+// the rti1516/Exception header misses that.
+#include <iosfwd>
+
+#include <RTI/NullFederateAmbassador.h>
+
+namespace rti1516
+{
+
+}
