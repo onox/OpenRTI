2d4169987456eccb287370efb728f7dd20ad24d4
Mathias Froehlich <Mathias.Froehlich@web.de>
2014-05-01 09:02:40 +0200

    Add test to see if linking to rti and fedtime library is sufficient.

---
+++ tests/RTI13/link/link.cpp
@@ -0,0 +1,33 @@
+/* -*-c++-*- OpenRTI - Copyright (C) 2013-2014 Mathias Froehlich
+ *
+ * This file is part of OpenRTI.
+ *
+ * OpenRTI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenRTI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpenRTI.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <cstdlib>
+#include <memory>
+#include <string>
+
+#include <RTI.hh>
+#include <fedtime.hh>
+
+int
+main(int argc, char* argv[])
+{
+  std::auto_ptr<RTI::RTIambassador> ambassador;
+  ambassador.reset(new RTI::RTIambassador);
+  return EXIT_SUCCESS;
+}
---
+++ tests/rti1516/link/link.cpp
@@ -0,0 +1,35 @@
+/* -*-c++-*- OpenRTI - Copyright (C) 2013-2014 Mathias Froehlich
+ *
+ * This file is part of OpenRTI.
+ *
+ * OpenRTI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenRTI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpenRTI.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <cstdlib>
+#include <memory>
+#include <string>
+
+#include <RTI/RTIambassadorFactory.h>
+#include <RTI/RTIambassador.h>
+
+int
+main(int argc, char* argv[])
+{
+  std::auto_ptr<rti1516::RTIambassador> ambassador;
+  rti1516::RTIambassadorFactory factory;
+  std::vector<std::wstring> args;
+  ambassador = factory.createRTIambassador(args);
+  return EXIT_SUCCESS;
+}
