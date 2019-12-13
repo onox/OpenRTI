29cecc850761465ad8bd2982535f644adf6d96bf
Mathias Froehlich <Mathias.Froehlich@web.de>
2019-12-13 10:56:34 +0100

    python: Allow subclassing of RTIambassador.
    Using that enable the pattern of deriving from the RTIambassador and
    providing the derived object instance also as a FederateAmbassador.
    The constraints to tp_dealloc when using this flag like mentioned
    in the python documentation holds.

--- python/rti1516e/Module.cpp
+++ python/rti1516e/Module.cpp
@@ -7260,7 +7260,7 @@ static PyTypeObject PyRTIambassadorType = {
   0,                                /* tp_getattro */
   0,                                /* tp_setattro */
   0,                                /* tp_as_buffer */
-  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC, /* tp_flags */
+  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC | Py_TPFLAGS_BASETYPE, /* tp_flags */
   "RTIambassador",                  /* tp_doc */
   (traverseproc)PyRTIambassadorObject_traverse, /* tp_traverse */
   (inquiry)PyRTIambassadorObject_clear, /* tp_clear */
